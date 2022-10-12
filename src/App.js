801b69aaf6c795991c2365339eaefa06204f5eb0
lauryn-21 <100782561+lauryn-21@users.noreply.github.com>
2022-10-12 15:12:12 +0300

    new codes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import { BrowserRouter, Routes, Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Carousel from './components/Carousel';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Navbar />
+      <Carousel/>
       <Footer/>
       <Routes>
-        <Route path='/' element={Navbar} />
+        <Route path='navbar' element={Navbar} />
+        <Route path='carousel' element={Carousel} />
+        <Route path='footer' element={Footer} />
+
+
       </Routes>
       </BrowserRouter>
     </div>
